57a3cebfd1c8a61e9ba58c5441b79c574a43175c
Roman Bolshakov <rombolshak@gmail.com>
2023-04-27 20:56:53 +0300

    standalone component bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   selector: 'dacker-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
