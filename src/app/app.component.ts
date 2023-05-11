367fa085e19fb1236f1259045265ce022232fc68
Roman Bolshakov <roman.bolshakov@kaspersky.com>
2023-05-11 12:09:12 +0300

    firebase init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './layout/menu/menu.component';
 import { TuiRootModule } from '@taiga-ui/core';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   standalone: true,
@@ -11,6 +14,22 @@ import { TuiRootModule } from '@taiga-ui/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dacker';
+
+  public ngOnInit() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDQaCfBRjlZh1BPpJNuQORrUA8VRbP4AGA",
+      authDomain: "bububudget.firebaseapp.com",
+      projectId: "bububudget",
+      storageBucket: "bububudget.appspot.com",
+      messagingSenderId: "143725096957",
+      appId: "1:143725096957:web:4c461cee6172b95e10b394",
+      measurementId: "G-6S9NTNHK18"
+    };
+
+// Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
