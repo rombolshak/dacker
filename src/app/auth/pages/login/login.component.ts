32df790d30f025cda8f89ceb840466a5e8fba852
Roman Bolshakov <rombolshak@gmail.com>
2023-05-12 02:14:57 +0300

    login page form

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/kit';
+import { TuiButtonModule, TuiLinkModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'monitraks-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiInputPasswordModule,
+    TuiButtonModule,
+    TuiLinkModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less'],
 })
