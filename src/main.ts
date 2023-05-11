f8184e861277eed749abcc05d107e37f4922cbdb
Roman Bolshakov <rombolshak@gmail.com>
2023-05-11 15:51:39 +0300

    menu colors and logo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
+import {TUI_SANITIZER} from "@taiga-ui/core";
+import {NgDompurifySanitizer} from "@tinkoff/ng-dompurify";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [provideRouter(routes), provideAnimations(), {
+    provide: TUI_SANITIZER,
+    useClass: NgDompurifySanitizer
+  }],
 }).catch(err => console.error(err));
