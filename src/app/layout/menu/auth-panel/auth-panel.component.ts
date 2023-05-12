cb08d7252137465dbec6dd3eb40c3c39b87cd127
Roman Bolshakov <rombolshak@gmail.com>
2023-05-12 18:10:58 +0300

    auth menu panel

--- src/app/layout/menu/auth-panel/auth-panel.component.ts
+++ src/app/layout/menu/auth-panel/auth-panel.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../../auth/services/auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from '@app/auth/services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { TuiDataListModule, TuiSvgModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'monitraks-auth-panel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiSvgModule, TuiDataListModule, RouterLink],
   templateUrl: './auth-panel.component.html',
   styleUrls: ['./auth-panel.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthPanelComponent {
   constructor(public auth: AuthService, private router: Router) {}
--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { TuiSvgModule } from '@taiga-ui/core';
+import { TuiHostedDropdownModule, TuiSvgModule } from '@taiga-ui/core';
 import { AuthPanelComponent } from './auth-panel/auth-panel.component';
+import { TuiArrowModule, tuiArrowOptionsProvider } from '@taiga-ui/kit';
 
 @Component({
   selector: 'monitraks-menu',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, TuiSvgModule, AuthPanelComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    TuiSvgModule,
+    AuthPanelComponent,
+    TuiHostedDropdownModule,
+    TuiArrowModule,
+  ],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.less'],
+  providers: [tuiArrowOptionsProvider({ iconLarge: 'tuiIconChevronUpLarge', iconSmall: 'tuiIconChevronUp' })],
 })
 export class MenuComponent {
   menuItems = [
