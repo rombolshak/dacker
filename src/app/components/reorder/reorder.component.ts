1c16167b17e2083c960ec54c5c6e19738258e101
Roman Bolshakov <rombolshak@gmail.com>
2023-08-31 00:43:02 +0300

    prettier

--- src/app/components/reorder/reorder.component.ts
+++ src/app/components/reorder/reorder.component.ts
@@ -1,129 +1,113 @@
-import {
-    ChangeDetectionStrategy,
-    Component,
-    EventEmitter,
-    HostListener,
-    Inject,
-    Input,
-    Output,
-} from '@angular/core';
-import {TUI_TABLE_SHOW_HIDE_MESSAGE} from '@taiga-ui/addon-table/tokens';
-import {Observable} from 'rxjs';
+import { ChangeDetectionStrategy, Component, EventEmitter, HostListener, Inject, Input, Output } from '@angular/core';
+import { TUI_TABLE_SHOW_HIDE_MESSAGE } from '@taiga-ui/addon-table/tokens';
+import { Observable } from 'rxjs';
 
-import {TUI_REORDER_OPTIONS, TuiReorderOptions} from './reorder.options';
+import { TUI_REORDER_OPTIONS, TuiReorderOptions } from './reorder.options';
 
 @Component({
-    selector: 'tui-reorder',
-    templateUrl: './reorder.template.html',
-    styleUrls: ['./reorder.style.less'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'tui-reorder',
+  templateUrl: './reorder.template.html',
+  styleUrls: ['./reorder.style.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TuiReorderComponent<T> {
-    private dragging = false;
+  private dragging = false;
 
-    @Input()
-    set items(items: readonly T[]) {
-        if (
-            items.length !== this.unsortedItems.length ||
-            !items.every(item => this.unsortedItems.includes(item))
-        ) {
-            this.unsortedItems = items;
-        }
+  @Input()
+  set items(items: readonly T[]) {
+    if (items.length !== this.unsortedItems.length || !items.every(item => this.unsortedItems.includes(item))) {
+      this.unsortedItems = items;
+    }
+  }
+
+  @Input()
+  enabled: readonly T[] = [];
+
+  @Output()
+  readonly itemsChange = new EventEmitter<T[]>();
+
+  @Output()
+  readonly enabledChange = new EventEmitter<T[]>();
+
+  order = new Map<number, number>();
+
+  unsortedItems: readonly T[] = [];
+
+  constructor(
+    @Inject(TUI_REORDER_OPTIONS) readonly options: TuiReorderOptions,
+    @Inject(TUI_TABLE_SHOW_HIDE_MESSAGE) readonly showHideText$: Observable<string>,
+  ) {}
+
+  @HostListener('focusout.stop')
+  noop(): void {}
+
+  @HostListener('pointerdown.silent')
+  onDrag(): void {
+    this.dragging = true;
+  }
+
+  @HostListener('document:pointerup.silent')
+  onDrop(): void {
+    if (!this.dragging) {
+      return;
     }
 
-    @Input()
-    enabled: readonly T[] = [];
+    this.dragging = false;
+    this.updateItems();
+  }
 
-    @Output()
-    readonly itemsChange = new EventEmitter<T[]>();
+  isEnabled(item: T): boolean {
+    return this.enabled.includes(item);
+  }
 
-    @Output()
-    readonly enabledChange = new EventEmitter<T[]>();
+  getIcon(item: T): string {
+    return this.isEnabled(item) ? this.options.icons.hide : this.options.icons.show;
+  }
 
-    order = new Map<number, number>();
+  toggle(toggled: T): void {
+    this.enabled = this.isEnabled(toggled)
+      ? this.enabled.filter(item => item !== toggled)
+      : this.enabled.concat(toggled);
 
-    unsortedItems: readonly T[] = [];
+    this.updateEnabled();
+  }
 
-    constructor(
-        @Inject(TUI_REORDER_OPTIONS) readonly options: TuiReorderOptions,
-        @Inject(TUI_TABLE_SHOW_HIDE_MESSAGE) readonly showHideText$: Observable<string>,
-    ) {}
+  move(index: number, direction: number): void {
+    const oldIndex = this.order.get(index) ?? index;
 
-    @HostListener('focusout.stop')
-    noop(): void {}
-
-    @HostListener('pointerdown.silent')
-    onDrag(): void {
-        this.dragging = true;
+    if ((!oldIndex && direction < 0) || (oldIndex === this.unsortedItems.length - 1 && direction > 0)) {
+      return;
     }
 
-    @HostListener('document:pointerup.silent')
-    onDrop(): void {
-        if (!this.dragging) {
-            return;
-        }
+    const newIndex = oldIndex + direction;
+    const oldItem = Array.from(this.order.values()).findIndex(item => item === newIndex);
 
-        this.dragging = false;
-        this.updateItems();
-    }
+    this.order.set(index, newIndex);
+    this.order.set(oldItem, oldIndex);
+    this.order = new Map(this.order);
 
-    isEnabled(item: T): boolean {
-        return this.enabled.includes(item);
-    }
+    this.updateItems();
+  }
 
-    getIcon(item: T): string {
-        return this.isEnabled(item) ? this.options.icons.hide : this.options.icons.show;
-    }
+  private getSortedItems(): T[] {
+    const items = new Array(this.unsortedItems.length);
 
-    toggle(toggled: T): void {
-        this.enabled = this.isEnabled(toggled)
-            ? this.enabled.filter(item => item !== toggled)
-            : this.enabled.concat(toggled);
+    this.unsortedItems.forEach((item, index) => {
+      items[this.order.get(index) ?? index] = item;
+    });
 
-        this.updateEnabled();
-    }
+    return items;
+  }
 
-    move(index: number, direction: number): void {
-        const oldIndex = this.order.get(index) ?? index;
+  private updateItems(): void {
+    this.itemsChange.emit(this.getSortedItems());
+    this.updateEnabled();
+  }
 
-        if (
-            (!oldIndex && direction < 0) ||
-            (oldIndex === this.unsortedItems.length - 1 && direction > 0)
-        ) {
-            return;
-        }
+  private updateEnabled(): void {
+    const enabled = this.getSortedItems().filter(item => this.isEnabled(item));
 
-        const newIndex = oldIndex + direction;
-        const oldItem = Array.from(this.order.values()).findIndex(
-            item => item === newIndex,
-        );
-
-        this.order.set(index, newIndex);
-        this.order.set(oldItem, oldIndex);
-        this.order = new Map(this.order);
-
-        this.updateItems();
-    }
-
-    private getSortedItems(): T[] {
-        const items = new Array(this.unsortedItems.length);
-
-        this.unsortedItems.forEach((item, index) => {
-            items[this.order.get(index) ?? index] = item;
-        });
-
-        return items;
-    }
-
-    private updateItems(): void {
-        this.itemsChange.emit(this.getSortedItems());
-        this.updateEnabled();
-    }
-
-    private updateEnabled(): void {
-        const enabled = this.getSortedItems().filter(item => this.isEnabled(item));
-
-        this.enabled = enabled;
-        this.enabledChange.emit(enabled);
-    }
+    this.enabled = enabled;
+    this.enabledChange.emit(enabled);
+  }
 }
