6e36fea8edb509d5ed92011791443f5edb11f162
Roman Bolshakov <roman.bolshakov@kaspersky.com>
2024-02-08 17:49:52 +0300

    fix test

--- src/app/pages/dashboard/dashboard/accounts-table/accounts-table.component.spec.ts
+++ src/app/pages/dashboard/dashboard/accounts-table/accounts-table.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountsTableComponent } from './accounts-table.component';
+import { StorageService } from '@app/data-layer/storage.service';
+import { FakeStorageService } from '@app/data-layer/fake-storage.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('AccountsTableComponent', () => {
   let component: AccountsTableComponent;
@@ -9,6 +12,12 @@ describe('AccountsTableComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AccountsTableComponent],
+      providers: [
+        {
+          provide: StorageService,
+          useClass: FakeStorageService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AccountsTableComponent);
